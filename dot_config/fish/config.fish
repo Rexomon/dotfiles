26a26fcfb1a4a29f9430a26e8b354bfadb85022a
Rexomon <muhammadindriyo12@gmail.com>
2026-05-23 12:41:24 +0700

    Refine Oh My Posh theme and optimize fish config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,6 +1,9 @@
 # Disable fish greeting message
 set -U fish_greeting
 
+# Set specific GPU for Zed
+set --export ZED_DEVICE_ID 1347
+
 # Load Oh My Posh configuration
 oh-my-posh init fish --config ~/.config/OhMyPosh/rexudud.json | source
 
@@ -9,10 +12,14 @@ alias ff="fastfetch"
 alias yayq="yay -Qs"
 alias yayr="yay -Rns"
 alias yays="yay -Scc"
+alias ls="eza --icons --color=always --icons=always"
 
 # Load fastfetch on start
-ff
+if status is-interactive
+  ff
+end
 
+# List directory contents when changing directories
 function chwpd_handler --on-variable PWD
   ls
 end
@@ -23,6 +30,14 @@ set --export PATH "$BUN_INSTALL/bin:$PATH"
 
 # Set up fzf key bindings and fuzzy completion
 fzf --fish | source
+
+set --export FZF_DEFAULT_COMMAND "fd --hidden --strip-cwd-prefix --exclude .git"
+set --export FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+set --export FZF_ALT_C_COMMAND "fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+
+set --export FZF_DEFAULT_OPTS "--height 50% --layout=default --border"
+set --export FZF_CTRL_T_OPTS "--preview 'bat --color=always -n --line-range :500 {}'"
+set --export FZF_ALT_C_OPTS "--preview 'ls --tree --color=always {} | head -n 200'"
 
 # zoxide
 export _ZO_DOCTOR='0'
